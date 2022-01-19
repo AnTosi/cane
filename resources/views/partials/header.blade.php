40f2361e930fca1fcfa1155aea7e9676980e5a36
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-19 16:58:39 +0100

    create view (definitely everything from scrap)

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,35 @@
+<header id="site_header">
+    <div class="bg_main-blue upper_band">
+        <div class="d-flex justify-content-end py-2 container">
+            <div class="px-5 semi-bold"> 
+                DC POWER&#8480; VISA&#174;
+            </div>
+            <div class="semi-bold">
+                ADDITIONAL DC SITES <span class="half-rem">&#9660;</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="container d-flex justify-content-between align-items-center nav-container">
+            <img src="{{'../img/dc-logo.png'}}" alt="">
+            <nav>
+                <ul class="d-flex">
+                    <li class="mx-3 semi-bold"><a href="#">CHARACTERS</a></li>
+                    <li class="mx-3 semi-bold active"><a href="/">COMICS</a></li>
+                    <li class="mx-3 semi-bold"><a href="#">MOVIES</a></li>
+                    <li class="mx-3 semi-bold"><a href="#">TV</a></li>
+                    <li class="mx-3 semi-bold"><a href="#">GAMES</a></li>
+                    <li class="mx-3 semi-bold"><a href="#">COLLECTIBLES</a></li>
+                    <li class="mx-3 semi-bold"><a href="#">VIDEOS</a></li>
+                    <li class="mx-3 semi-bold"><a href="#">FANS</a></li>
+                    <li class="mx-3 semi-bold"><a href="#">NEWS</a></li>
+                    <li class="mx-3 semi-bold"><a href="#">SHOP <span class="text_main-blue half-rem">&#9660;</span></a></li>
+                </ul>
+            </nav>
+            <div class="semi-bold search-bar">
+                Search <i class="fas fa-search"></i>
+            </div>
+    </div>
+
+    
+</header>
