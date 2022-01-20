e374bab219f3d5225466fa7da9d5bfa9015777bc
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-20 15:55:45 +0100

    add persistent field in form

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -8,7 +8,7 @@
           @csrf
           <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" name="title" id="title" class="form-control @error('title') is-invalid @enderror" placeholder="Type your title here" aria-describedby="titleHelper">
+            <input type="text" name="title" id="title" class="form-control @error('title') is-invalid @enderror" placeholder="Type your title here" aria-describedby="titleHelper" value="{{old('title')}}">
             <small id="titleHelper" class="text-muted">Type a title for your post, max: 255 characters</small>
             {{-- @if ($errors->any())
               <div class="alert alert-danger">
@@ -26,7 +26,7 @@
           </div>
           <div class="mb-3">
             <label for="text" class="form-label">Text</label>
-            <textarea class="form-control @error('text') is-invalid @enderror" name="text" id="text" rows="5"></textarea>
+            <textarea class="form-control @error('text') is-invalid @enderror" name="text" id="text" rows="5">{{old('text')}}</textarea>
             @error('text')
               <div class="alert alert-danger">{{ $message }}</div>
             @enderror
