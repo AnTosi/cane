5ccfabf8b1fbd46ca424ff45cbbbb39e88602de9
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-21 15:52:26 +0100

    finished dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,38 +4,24 @@
 
     <h1>Welcome to the dashboard</h1>
 
-    <div class="d-flex flex-column flex-shrink-0 p-3 text_main-blue" style="width: 280px;">
-        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text_main-blue text-decoration-none">
-          <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-          <span class="fs-4">Sidebar</span>
-        </a>
-        <hr>
-        <ul class="nav nav-pills flex-column mb-auto">
-          <li class="nav-item bg_main-blue"">
-            <a href="#" class="nav-link active" aria-current="page">
-              Dashboard
-            </a>
-          </li>
-          <li class="bg_main-blue">
-            <a href="#" class="nav-link text-white">
-              Dashboard
-            </a>
-          </li>
-          <li class="bg_main-blue">
-            <a href="#" class="nav-link text-white">
-              Orders
-            </a>
-          </li>
-          <li class="bg_main-blue">
-            <a href="#" class="nav-link text-white">
-              Products
-            </a>
-          </li>
-          <li class="bg_main-blue">
-            <a href="#" class="nav-link text-white">
-              Customers
-            </a>
-          </li>
-        </ul>
-      </div>
+<div class="d-flex flex-column flex-shrink-0 p-3 text_main-blue" style="width: 280px;">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text_main-blue text-decoration-none">
+        <span class="fs-4">Dashboard</span>
+    </a>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item bg_main-blue">
+            <a href="{{route('admin.posts.index')}}" class="nav-link text-white" aria-current="page">Posts</a>
+        </li>
+        <li class="bg_main-blue">
+            <a href="#" class="nav-link text-white">Movies</a>
+        </li>
+        <li class="bg_main-blue">
+            <a href="#" class="nav-link text-white">Comics</a>
+        </li>
+        <li class="bg_main-blue">
+            <a href="#" class="nav-link text-white">Videos</a>
+        </li>
+    </ul>
+</div>
 @endsection
